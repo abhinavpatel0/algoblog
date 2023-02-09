70360811954d826e7834f9a0be2c57f0a0d390c9
Abhinavpatel <121427766+TheVeganAbhinav@users.noreply.github.com>
2023-02-09 08:31:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Header({ name }) {
+export default function Header({name="Blog" }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
